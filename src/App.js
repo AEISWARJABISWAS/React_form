faac59d5450015d8564469d3f14e7b924cee3445
Aeiswarja Biswas <aeiswarjabiswas98@gmail.com>
2024-05-08 00:12:33 +0530

    created form in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [userData, setUserData] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, password);
+    setUserData([{ name: name, password: password }, ...userData]);
+    console.log(userData);
+  };
+  const handleName = (e) => setName(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center" }}>
+      Registration Form
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Enter your UserName: </label>
+        <input
+          type="text"
+          id="username"
+          value={name}
+          onChange={handleName}
+          required
+        />
+        <br />
+        <label htmlFor="password">Enter password: </label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePassword}
+          required
+        />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      {userData.map((item) => (
+        <ol>
+          <li>{item.name}</li>
+          <li>{item.password}</li>
+        </ol>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
